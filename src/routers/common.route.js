73d24028132ab8d2decb9d1aabf06bab10cfc645
Rahul-Dev24 <149306097+Rahul-Dev24@users.noreply.github.com>
2025-01-28 19:11:45 +0530

    Delete src directory

--- src/routers/common.route.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { imageUpload } from "../controllers/userHomePage.controller.js";
-
-const commonRoute = express();
-
-commonRoute.post("/upload/image", imageUpload);
-
-export default commonRoute;
