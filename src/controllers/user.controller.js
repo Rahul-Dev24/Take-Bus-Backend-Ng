374838ca5fc6c3a9fd8d9246c2c41168e3f79297
Rahul-Dev24 <149306097+Rahul-Dev24@users.noreply.github.com>
2025-01-29 11:54:13 +0530

    edited

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,13 +3,13 @@ import User from "../models/user.model.js";
 
 export const updateUser = catchError(async (req, res, next) => {
     try {
-        const { name, phoneNo, email, address } = req.body;
+        const { name, phoneNo, email, address, profileImg } = req.body;
 
-        if (!name || !address || !role || !phoneNo || !email) return next(new ErrorHandeler("All fields are required.", 400));
+        if (!name || !address || !phoneNo || !email) return next(new ErrorHandeler("All fields are required.", 400));
 
         const user = await User.updateOne({ _id: req.user._id }, {
             $set: {
-                name, phoneNo, email, address
+                name, phoneNo, email, address,profileImg
             }
         });
         if (!user) return next(new ErrorHandeler("Something went wrong. Please login again.", 401));
