82f1c6c7023dd920e1597cc6e14e755942daa0bc
Rahul-Dev24 <149306097+Rahul-Dev24@users.noreply.github.com>
2025-01-29 14:49:35 +0530

    Delete src directory

--- src/routers/user.route.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { updateUser } from "../controllers/user.controller.js";
-import { auth } from "../middlewares/auth.middleware.js";
-
-const userRoute = express();
-
-userRoute.put("/updateUser", auth, updateUser);
-
-export default userRoute;
--- src/utils/genrateOTP.js
+++
@@ -1,5 +0,0 @@
-export const generateOtp = () => {
-    const firstNum = Math.floor(Math.random() * 9) + 1;
-    const remaindNum = Math.floor(Math.random() * 10000).toString().padStart(4, 0);
-    return parseInt(firstNum + remaindNum);
-}
