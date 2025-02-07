aa15547b36356d9310a3b942dc9c3b7dbd09cd52
Rahul-Dev24 <149306097+Rahul-Dev24@users.noreply.github.com>
2025-02-07 16:03:56 +0530

    Delete src directory

--- src/utils/DBConnection.js
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose";
-
-export const DBConnection = () => {
-    mongoose.connect(process.env.MONGO_DB_URL, {
-        dbName: "Take_Bus_DB"
-    }).then(() => console.log("DBConnected")).catch(() => console.log("DB not Connected"))
-}
