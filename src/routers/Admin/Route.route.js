7e1ab64555a1ab9d045569d5b49609b5fd1a519c
Rahul <rahul.s@engage2serve.com>
2025-02-25 15:15:39 +0530

    Spaling mistake

--- src/routers/Admin/Route.route.js
+++ src/routers/Admin/Route.route.js
@@ -6,6 +6,6 @@ const routeRoute = express();
 routeRoute.get("/getRoute/:IP", getRoute);
 routeRoute.get("/getRoute", getRoute);
 routeRoute.get("/searchRoute/:query", searchRoute);
-routeRoute.get("/searchRout", searchRoute);
+routeRoute.get("/searchRoute", searchRoute);
 
 export default routeRoute;
